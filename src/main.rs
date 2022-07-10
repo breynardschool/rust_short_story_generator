1c74b3b5404385f25f1236ced1ce853af37b8566
breynardschool <105508253+breynardschool@users.noreply.github.com>
2022-07-10 00:23:00 +0000

    Finished! Probably...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 use rust_short_story_generator::words::words::*;
+use rand::prelude::*;
+
+const SENTENCE_COUNT: i32 = 4;
 
 fn main() 
 {
-    
+    let mut sentences: Vec<Token> = Vec::new();
+
+    let crabbattles = include_str!("./resources/inputfiles/crabbattles.txt");
+    let sickking = include_str!("./resources/inputfiles/sickking.txt");
+    let star = include_str!("./resources/inputfiles/star.txt");
+
+    let mut data: Vec<String> = Vec::new();
+
+    for s in crabbattles.split(". ") {
+        data.push(s.to_string());
+    }
+
+    for s in sickking.split(". ") {
+        data.push(s.to_string());
+    }
+
+    for s in star.split(". ") {
+        data.push(s.to_string());
+    }
+
+    // Lexing
+    let mut i = 0;
+    while i < SENTENCE_COUNT {
+        let cur_sentence = &data[thread_rng().gen_range(0..data.len())];
+
+        let tokens = lexer(cur_sentence.to_string());
+        for t in tokens {
+            sentences.push(t);
+        }
+
+        i += 1;
+    }
+
+    let story = parser(sentences).unwrap();
+    println!("{}", story);
 }
--- src/words.rs
+++ src/words.rs
@@ -1,16 +1,8 @@
 pub mod words
 {
     use std::char;
-
-    pub enum WordType
-    {
-        ADJECTIVE,
-        VERB,
-        NOUN,
-        TRANSITION,
-        ADVERB,
-        NAME
-    }
+    use rand::prelude::*;
+    use rand_chacha::ChaCha8Rng;
 
     #[derive(Debug)]
     pub enum Token
@@ -301,59 +293,315 @@ pub mod words
 
         tokens
     }
+
+    pub fn parser(tokens: Vec<Token>) -> Result<String, &'static str> {
+        let mut output = String::new();
+        let mut tokens = tokens;
+
+        let nouns_raw = include_str!("./resources/wordslist/nouns.txt").to_string();
+        let mut nouns: Vec<String> = Vec::new();
+        for f in nouns_raw.split("\n") {
+            nouns.push(f.to_string());
+        }
+
+        let adj_raw = include_str!("./resources/wordslist/adjectives.txt").to_string();
+        let mut adj_list: Vec<String> = Vec::new();
+        for f in adj_raw.split("\n") {
+            adj_list.push(f.to_string());
+        }
+
+        let adverbs_raw = include_str!("./resources/wordslist/adverbs.txt").to_string();
+        let mut adverbs: Vec<String> = Vec::new();
+        for f in adverbs_raw.split("\n") {
+            adverbs.push(f.to_string());
+        }
+
+        let past_verbs_raw = include_str!("./resources/wordslist/verbs/past.txt").to_string();
+        let mut past_verbs: Vec<String> = Vec::new();
+        for f in past_verbs_raw.split("\n") {
+            past_verbs.push(f.to_string());
+        }
+
+        let present_verbs_raw = include_str!("./resources/wordslist/verbs/present.txt").to_string();
+        let mut present_verbs: Vec<String> = Vec::new();
+        for f in present_verbs_raw.split("\n") {
+            present_verbs.push(f.to_string());
+        }
+
+        let names_raw = include_str!("./resources/wordslist/names/surnames/names.txt").to_string();
+        let mut names: Vec<String> = Vec::new();
+        for f in names_raw.split("\n") {
+            names.push(f.to_string());
+        }
+
+        let whitelist_raw = include_str!("./resources/wordslist/whitelist.txt").to_string();
+        let mut whitelist: Vec<String> = Vec::new();
+        for f in whitelist_raw.split("\n") {
+            whitelist.push(f.to_string());
+        }
+
+        for token in &mut tokens {
+            match token {
+                Token::NUMBER(num) => {
+                    *num = thread_rng().gen_range(0..*num * 2);
+                    output = format!("{}{}", output, " ");
+                    output = format!("{}{}", output, num.to_string());
+                }
+                Token::WORD(word) => {
+                    if whitelist.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, word);
+                        continue;
+                    }
+
+                    if adverbs.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, rand_adverb());
+                        continue;
+                    }
+
+                    if adj_list.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, rand_adj());
+                        continue;
+                    }
+
+                    if past_verbs.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, rand_verb_past());
+                        continue;
+                    }
+
+                    if present_verbs.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, rand_verb_present());
+                        continue;
+                    }
+
+                    if nouns.contains(&word.to_lowercase()) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, rand_noun());
+                        continue;
+                    }
+
+                    if nouns.contains(&format!("{}s", &word.to_lowercase())) {
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}s", output, rand_noun());
+                        continue;
+                    }
+
+                    if names.contains(&word.to_lowercase()) {
+                        println!("Swapping name: {}", word);
+                        output = format!("{}{}", output, " ");
+                        output = format!("{}{}", output, word);
+                        continue;
+                    }
+
+                    output = format!("{}{}", output, " ");
+                    output = format!("{}{}", output, word);
+
+                    continue;
+                }
+                Token::INDENT => {
+                    output = format!("{}{}", output, "  ");
+                    continue;
+                }
+                Token::COMMA => {
+                    output = format!("{}{}", output, ",");
+                    continue;
+                }
+                Token::DIALOGUESTART => {
+                    output = format!("{}{}", output, " ");
+                    output = format!("{}{}", output, "\"");
+                    continue;
+                }
+                Token::DIALOGUEEND => {
+                    output = format!("{}{}", output, "\"");
+                    continue;
+                }
+                Token::EXCLAMATION => {
+                    output = format!("{}{}", output, "!");
+                    continue;
+                }
+                Token::PERIOD => {
+                    output = format!("{}{}", output, ".");
+                    continue;
+                }
+                Token::QUESTION => {
+                    output = format!("{}{}", output, "?");
+                    continue;
+                }
+                Token::SPECIALCHAR(c) => {
+                    output = format!("{}{}", output, c);
+                    continue;
+                }
+            }
+        }
+
+        output = output.replace("\' ", "\'");
+        output = output.replace("\" ", "\"");
+
+        output = output.replace(". a", ". A");
+        output = output.replace(". b", ". B");
+        output = output.replace(". c", ". C");
+        output = output.replace(". d", ". D");
+        output = output.replace(". e", ". E");
+        output = output.replace(". f", ". F");
+        output = output.replace(". g", ". G");
+        output = output.replace(". h", ". H");
+        output = output.replace(". i", ". I");
+        output = output.replace(". j", ". J");
+        output = output.replace(". k", ". K");
+        output = output.replace(". l", ". L");
+        output = output.replace(". m", ". M");
+        output = output.replace(". n", ". N");
+        output = output.replace(". o", ". O");
+        output = output.replace(". p", ". P");
+        output = output.replace(". q", ". Q");
+        output = output.replace(". r", ". R");
+        output = output.replace(". s", ". S");
+        output = output.replace(". t", ". T");
+        output = output.replace(". u", ". U");
+        output = output.replace(". v", ". V");
+        output = output.replace(". w", ". W");
+        output = output.replace(". x", ". X");
+        output = output.replace(". y", ". Y");
+        output = output.replace(". z", ". Z");
+
+        output = output.replace("? a", "? A");
+        output = output.replace("? b", "? B");
+        output = output.replace("? c", "? C");
+        output = output.replace("? d", "? D");
+        output = output.replace("? e", "? E");
+        output = output.replace("? f", "? F");
+        output = output.replace("? g", "? G");
+        output = output.replace("? h", "? H");
+        output = output.replace("? i", "? I");
+        output = output.replace("? j", "? J");
+        output = output.replace("? k", "? K");
+        output = output.replace("? l", "? L");
+        output = output.replace("? m", "? M");
+        output = output.replace("? n", "? N");
+        output = output.replace("? o", "? O");
+        output = output.replace("? p", "? P");
+        output = output.replace("? q", "? Q");
+        output = output.replace("? r", "? R");
+        output = output.replace("? s", "? S");
+        output = output.replace("? t", "? T");
+        output = output.replace("? u", "? U");
+        output = output.replace("? v", "? V");
+        output = output.replace("? w", "? W");
+        output = output.replace("? x", "? X");
+        output = output.replace("? y", "? Y");
+        output = output.replace("? z", "? Z");
+
+        output = output.replace("! a", "! A");
+        output = output.replace("! b", "! B");
+        output = output.replace("! c", "! C");
+        output = output.replace("! d", "! D");
+        output = output.replace("! e", "! E");
+        output = output.replace("! f", "! F");
+        output = output.replace("! g", "! G");
+        output = output.replace("! h", "! H");
+        output = output.replace("! i", "! I");
+        output = output.replace("! j", "! J");
+        output = output.replace("! k", "! K");
+        output = output.replace("! l", "! L");
+        output = output.replace("! m", "! M");
+        output = output.replace("! n", "! N");
+        output = output.replace("! o", "! O");
+        output = output.replace("! p", "! P");
+        output = output.replace("! q", "! Q");
+        output = output.replace("! r", "! R");
+        output = output.replace("! s", "! S");
+        output = output.replace("! t", "! T");
+        output = output.replace("! u", "! U");
+        output = output.replace("! v", "! V");
+        output = output.replace("! w", "! W");
+        output = output.replace("! x", "! X");
+        output = output.replace("! y", "! Y");
+        output = output.replace("! z", "! Z");
+
+        Ok(output)
+    }
     
     pub fn rand_noun() -> String
     {
-        use std::{
-            fs::File,
-            io::{prelude::*, BufReader}
-        };
-
         use rand::prelude::*;
 
-        let file = File::open("./src/resources/wordslist/nouns.txt").expect("no such file");
-        let buf = BufReader::new(file);
-        let lines: Vec<String> = buf.lines()
-            .map(|l| l.expect("Could not parse line"))
-            .collect();
+        let file = include_str!("./resources/wordslist/nouns.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
         
-        lines.to_vec()[thread_rng().gen_range(0..1525)].to_string()
+        lines.to_vec()[thread_rng().gen_range(0..lines.len())].to_string()
     }
 
     pub fn rand_adj() -> String
     {
-        use std::{
-            fs::File,
-            io::{prelude::*, BufReader}
-        };
-
         use rand::prelude::*;
 
-        let file = File::open("./src/resources/wordslist/adjectives.txt").expect("no such file");
-        let buf = BufReader::new(file);
-        let lines: Vec<String> = buf.lines()
-            .map(|l| l.expect("Could not parse line"))
-            .collect();
+        let file = include_str!("./resources/wordslist/adjectives.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
         
-        lines.to_vec()[thread_rng().gen_range(0..1347)].to_string()
+        lines.to_vec()[thread_rng().gen_range(0..lines.len())].to_string()
     }
 
-    pub fn rand_name() -> String
+    pub fn rand_adverb() -> String
     {
-        use std::{
-            fs::File,
-            io::{prelude::*, BufReader}
-        };
-
         use rand::prelude::*;
 
-        let file = File::open("./src/resources/wordslist/names/surnames/english.txt").expect("no such file");
-        let buf = BufReader::new(file);
-        let lines: Vec<String> = buf.lines()
-            .map(|l| l.expect("Could not parse line"))
-            .collect();
+        let file = include_str!("./resources/wordslist/adverbs.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
         
-        let mut name = lines.to_vec()[thread_rng().gen_range(0..671)].to_string();
+        lines.to_vec()[thread_rng().gen_range(0..lines.len())].to_string()
+    }
+
+    pub fn rand_verb_past() -> String
+    {
+        use rand::prelude::*;
+
+        let file = include_str!("./resources/wordslist/verbs/past.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
+        
+        lines.to_vec()[thread_rng().gen_range(0..lines.len())].to_string()
+    }
+
+    pub fn rand_verb_present() -> String
+    {
+        use rand::prelude::*;
+
+        let file = include_str!("./resources/wordslist/verbs/present.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
+        
+        lines.to_vec()[thread_rng().gen_range(0..lines.len())].to_string()
+    }
+
+    pub fn rand_name(idx: u64) -> String
+    {
+        use rand::prelude::*;
+
+        let file = include_str!("./resources/wordslist/names/surnames/names.txt").to_string();
+        let mut lines: Vec<String> = Vec::new();
+        for f in file.split("\n") {
+            lines.push(f.to_string());
+        }
+        
+        let mut rand = ChaCha8Rng::seed_from_u64(idx);
+        let mut name = lines.to_vec()[rand.gen_range(0..lines.len())].to_string();
         name.replace_range(0..1, &name.chars().nth(0).unwrap().to_string().as_str().to_uppercase());
         name
     }
